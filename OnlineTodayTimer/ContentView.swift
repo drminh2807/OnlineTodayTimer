d5662e19e9cf1ed6416b6610a752ace1aff3f542
minhdv <minhdv@pinetree.vn>
2023-12-20 09:20:41 +0700

    Initial Commit

---
+++ OnlineTodayTimer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlineTodayTimer
+//
+//  Created by Doan Van Minh on 20/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlineTodayTimer/OnlineTodayTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineTodayTimerApp.swift
+//  OnlineTodayTimer
+//
+//  Created by Doan Van Minh on 20/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct OnlineTodayTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
